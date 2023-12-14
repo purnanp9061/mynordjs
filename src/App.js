631ebe4243b4f0adb837e59b31cb7f719803d256
purnanp9061 <153167963+purnanp9061@users.noreply.github.com>
2023-12-14 23:30:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hai
+          Panipata Lida PUKA , poye aidayina pani chisuko
         </p>
         <a
           className="App-link"
